f4259674d73268e87c8f2095e065654ee92f2799
hhdblog <hhdblog@gmail.com>
2021-05-03 15:45:48 +0300

    update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from flask import Flask
+import os
+from selenium import webdriver
+
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
+
+driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!dfgdfgdf'
+def index():
+	try:
+		driver.get('https://kartaca.com')
+            #browser.get_screenshot_as_file("/tmp/screenshot.png")
+            return(browser.title)
+        finally:
+        	browser.quit()
