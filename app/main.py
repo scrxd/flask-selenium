861d4b90f810da38623175a9b437136b951eff4e
hhdblog <hhdblog@gmail.com>
2021-05-03 16:29:38 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,22 +2,21 @@ import os
 from flask import Flask
 from selenium import webdriver
 
-op = webdriver.ChromeOptions()
-op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-op.add_argument("--headless")
-op.add_argument("--no-sandbox")
-op.add_argument("--disable-dev-sh-usage")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-sh-usage")
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     try:
         driver.get('https://kartaca.com')
         return driver.title
-        #return "24333333333333dfghgffghfg"
+        #return "test test"
     finally:
         print("sfdsf")
